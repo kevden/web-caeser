315e00f57e5aadbf3521ead803175909f1c9e1c2
Kevin Linden <kevinden@protonmail.com>
2017-01-25 18:42:19 -0600

    finished crypto?

--- caeser.py
+++ caeser.py
@@ -7,8 +7,16 @@ def alphabet_position(letter):
 def rotate_character(char, rot):
     if char.isalpha():
         if char.isupper():
-            return chr(alphabet_position(char) + int(rot) + 65)
+            return chr((alphabet_position(char) + int(rot))%26 + 65)
         else:
-            return chr(alphabet_position(char) + int(rot) + 97)
+            return chr((alphabet_position(char) + int(rot))%26+ 97)
+    else:
+        return char
 	
-print(rotate_character('A', 13))
+def encrypt(text, rot):
+    new_message= ''
+    for character in text:
+        new_character = rotate_character(character, rot)
+        new_message += new_character
+    return new_message
+	
